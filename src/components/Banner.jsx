1b68d921f010ebdcb7d973e8b10a325eedbffd40
River Kahrhoff <river.kahrhoff@gmail.com>
2024-08-24 09:06:55 -0500

    task 2 complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+function Banner() {
+    return (
+        <>
+        <header>
+            <h1>Orbit Report</h1>
+        </header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </> 
+    );
+};
+
+export default Banner;
